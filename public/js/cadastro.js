0794401b3f7751eefca01de6e382d22fdb6d8f67
uken49 <heldergavioes@hotmail.com>
2022-04-18 17:43:12 -0300

    Ajustando responsividade do header e do login. Criando validação de senha para o cadastro(84%).

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -2,26 +2,46 @@
 function seePass() {
     let eye = document.getElementById('i_eye_pass')
     let pass = document.getElementById('inp_pass')
-    
+
     if (pass.type == 'password') {
         eye.className = 'fa-solid fa-eye-slash'
         pass.type = 'text'
-    }else{
+    } else {
         pass.type = 'password'
         eye.className = 'fa-solid fa-eye'
     }
 }
 
-// Função para exibir/esconder a confirmação da senha
-function seePassConf() {
-    let eye = document.getElementById('i_eye_pass_conf')
-    let pass = document.getElementById('inp_pass_conf')
-    
-    if (pass.type == 'password') {
-        eye.className = 'fa-solid fa-eye-slash'
-        pass.type = 'text'
-    }else{
-        pass.type = 'password'
-        eye.className = 'fa-solid fa-eye'
+// Função para validar senha
+
+// Checando se a senha é válida
+function passCheck() {
+    let pass = document.getElementById('inp_pass').value
+
+    if (pass.length >= 6) {
+        warning_pass.innerHTML = ''
+        label_pass.className = 'label-float valid'
+    } else {
+        label_pass.className = 'label-float missing'
+        warning_pass.innerHTML = 'A senha deverá ter 6 ou mais digitos'
+    }
+}
+
+// Checando se as duas senhas são iguais
+function valPass() {
+    let pass = document.getElementById('inp_pass').value
+    let passConf = document.getElementById('inp_pass_conf').value
+
+    if (passConf.length >= 6) {
+        if (pass == passConf) {
+            label_pass_conf.className = 'label-float valid'
+            warning_pass_conf.innerHTML = ''
+        } else {
+            label_pass_conf.className = 'label-float invalid'
+            warning_pass_conf.innerHTML = 'As senhas não conferem'
+        }
+    } else {
+        label_pass_conf.className = 'label-float missing'
+        warning_pass_conf.innerHTML = 'Senha com menos de 6 digitos'
     }
 }

67f0803d5b4a93bf7f343e9a46c2d4a014479ed1
uken49 <heldergavioes@hotmail.com>
2022-04-17 22:59:52 -0300

    Finalizando o login (100%) e atualizando index (92%)

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+//Pegando o ano atual e colocando no rodapé
+const actDate = new Date();
+const actYear = actDate.getFullYear();
+
+spn_year.innerHTML = actYear
---
+++ public/js/login.js
@@ -0,0 +1,13 @@
+// Função para exibir/esconder a senha
+function seePass() {
+    let eye = document.getElementById('i_eye')
+    let pass = document.getElementById('inp_pass')
+    
+    if (pass.type == 'password') {
+        eye.className = 'fa-solid fa-eye-slash'
+        pass.type = 'text'
+    }else{
+        pass.type = 'password'
+        eye.className = 'fa-solid fa-eye'
+    }
+}

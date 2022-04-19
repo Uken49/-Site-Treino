5b532c31020d2713d13d91562ed5ff64c9d7d81f
uken49 <heldergavioes@hotmail.com>
2022-04-19 18:04:16 -0300

    Criando validação para os campos da tela de cadastro

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -17,13 +17,21 @@ function seePass() {
 // Checando se a senha é válida
 function passCheck() {
     let pass = document.getElementById('inp_pass').value
+    let regex = /^(?=.*[@!#$%^&*()/\\])[@!#$%^&*()/\\a-zA-Z0-9]{8,20}$/
 
-    if (pass.length >= 6) {
+    // Verificando se a senha é forte com regex
+    if (pass == '') {
+        warning_pass.innerHTML = 'Digite uma senha'
+        label_pass.className = 'label-float invalid'
+        return false
+    } else if (regex.test(pass)) {
         warning_pass.innerHTML = ''
         label_pass.className = 'label-float valid'
+        return true
     } else {
         label_pass.className = 'label-float missing'
-        warning_pass.innerHTML = 'A senha deverá ter 6 ou mais digitos'
+        warning_pass.innerHTML = 'Use oito ou mais caracteres com uma combinação de letras, números e símbolos: @ ! # $ % ^ & * ( ) / e \\'
+        return false
     }
 }
 
@@ -36,12 +44,146 @@ function valPass() {
         if (pass == passConf) {
             label_pass_conf.className = 'label-float valid'
             warning_pass_conf.innerHTML = ''
+            return true
         } else {
             label_pass_conf.className = 'label-float invalid'
             warning_pass_conf.innerHTML = 'As senhas não conferem'
+            return false
         }
     } else {
         label_pass_conf.className = 'label-float missing'
         warning_pass_conf.innerHTML = 'Senha com menos de 6 digitos'
+        return false
+    }
+}
+
+// Validando email
+function valEmail() {
+    let email = document.getElementById('inp_email').value
+    let regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi
+    // Essa expressão não garante a veracidade 100% de um email
+
+    // Validando email se os caracteres do email é válido
+    if (email == '') {
+        label_email.className = 'label-float invalid'
+        warning_email.innerHTML = 'Digite um email válido'
+        return false
+    } else if (regex.test(email)) {
+        warning_email.innerHTML = ''
+        label_email.className = 'label-float valid'
+        return true
+    } else {
+        label_email.className = 'label-float missing'
+        warning_email.innerHTML = 'Digite um email válido'
+        return false
+    }
+}
+
+// Validando nome
+function valName() {
+    let name = document.getElementById('inp_name').value
+    let regex = /^[a-z].* {1,}[a-z]{1,}/gi
+
+    // Validando a quantidade de palavra e caracteres
+    if (name == '') {
+        label_name.className = 'label-float invalid'
+        warning_name.innerHTML = 'Digite seu nome completo'
+        return false
+    }else if (regex.test(name)) {
+        warning_name.innerHTML = ''
+        label_name.className = 'label-float valid'
+        return true
+    } else {
+        label_name.className = 'label-float missing'
+        warning_name.innerHTML = 'Digite seu nome completo'
+        return false
+    }
+}
+
+// Validando CNPJ
+function valCnpj() {
+    var cnpj = document.getElementById('inp_cnpj').value;
+    cnpj = cnpj.replace(/[^\d]+/g, '');
+
+    if (cnpj == '') {
+        label_cnpj.className = 'label-float invalid'
+        warning_cnpj.innerHTML = 'Digite um CNPJ válido'
+        return false;
+    }
+
+    if (cnpj.length != 14) {
+        label_cnpj.className = 'label-float missing'
+        warning_cnpj.innerHTML = 'Digite um CNPJ válido'
+        return false;
+    }
+    // Elimina CNPJs invalidos conhecidos
+    if (cnpj == "00000000000000" ||
+        cnpj == "11111111111111" ||
+        cnpj == "22222222222222" ||
+        cnpj == "33333333333333" ||
+        cnpj == "44444444444444" ||
+        cnpj == "55555555555555" ||
+        cnpj == "66666666666666" ||
+        cnpj == "77777777777777" ||
+        cnpj == "88888888888888" ||
+        cnpj == "99999999999999") {
+
+        label_cnpj.className = 'label-float invalid'
+        warning_cnpj.innerHTML = 'Digite um CNPJ válido'
+        return false;
+    }
+
+    // Valida DVs
+    tamanho = cnpj.length - 2
+    numeros = cnpj.substring(0, tamanho);
+    digitos = cnpj.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+
+    if (resultado != digitos.charAt(0)) {
+        label_cnpj.className = 'label-float invalid'
+        warning_cnpj.innerHTML = 'Digite um CNPJ válido'
+        return false;
+    }
+
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+
+    for (i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2) {
+            pos = 9;
+        }
+    }
+
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+
+    if (resultado != digitos.charAt(1)) {
+        label_cnpj.className = 'label-float invalid'
+        warning_cnpj.innerHTML = 'Digite um CNPJ válido'
+        return false;
+    } else {
+        label_cnpj.className = 'label-float valid'
+        warning_cnpj.innerHTML = ''
+        return true;
+    }
+}
+
+function valSubmit() {
+    if (!valName() | !valCnpj() | !valEmail() | !passCheck() | !valPass()) {
+        alert("Erro ao enviar!!")
+        return false;
+    } else {
+        alert("Dados enviados com sucesso")
+        return true;
     }
 }

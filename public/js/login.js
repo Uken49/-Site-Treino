d30f51288b753563901e382035e524a392d0b14f
uken49 <heldergavioes@hotmail.com>
2022-04-26 17:18:04 -0300

    Validação Colocando a validação de email e de senha.

--- public/js/login.js
+++ public/js/login.js
@@ -2,12 +2,55 @@
 function seePass() {
     let eye = document.getElementById('i_eye')
     let pass = document.getElementById('inp_pass')
-    
+
     if (pass.type == 'password') {
         eye.className = 'fa-solid fa-eye'
         pass.type = 'text'
-    }else{
+    } else {
         pass.type = 'password'
         eye.className = 'fa-solid fa-eye-slash'
+    }
+}
+
+// Checando se senha é válida
+function passCheck() {
+    let pass = document.getElementById('inp_pass').value
+    let regex = /^(?=.*[@!#$%^&*()/\\])[@!#$%^&*()/\\a-zA-Z0-9]{8,20}$/
+
+    // Verificando se a senha é forte com regex
+    if (pass == '') {
+        warning_pass.innerHTML = 'Digite uma senha'
+        label_pass.className = 'label-float invalid'
+        return false
+    } else if (regex.test(pass)) {
+        warning_pass.innerHTML = ''
+        label_pass.className = 'label-float valid'
+        return true
+    } else {
+        label_pass.className = 'label-float missing'
+        warning_pass.innerHTML = 'Use oito ou mais caracteres com uma combinação de letras, números e símbolos: @ ! # $ % ^ & * ( ) / e \\'
+        return false
+    }
+}
+
+// Validando email
+function valEmail() {
+    let email = document.getElementById('inp_email').value
+    let regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi
+    // Essa expressão não garante a veracidade 100% de um email, para ser 100% precisa mandar confirmação por email
+
+    // Validando email se os caracteres do email é válido
+    if (email == '') {
+        label_email.className = 'label-float invalid'
+        warning_email.innerHTML = 'Digite um email válido'
+        return false
+    } else if (regex.test(email)) {
+        warning_email.innerHTML = ''
+        label_email.className = 'label-float valid'
+        return true
+    } else {
+        label_email.className = 'label-float missing'
+        warning_email.innerHTML = 'Digite um email válido'
+        return false
     }
 }
